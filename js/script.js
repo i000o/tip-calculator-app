a8ba7ed296f7a6c04e1af421ce74188a132bc56e
i000o <iosman1096@gmail.com>
2026-06-08 13:36:18 +0100

    reviewing original JS, comments

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => { // 1 wait til DOM is fully
 
     // 2 declare your variables 
 
+    // Change querySelectors to IDs for tailwind
+
     const billInput = document.querySelector(".bill"); // bill total input
     const tipInput = document.querySelector(".tip"); // select tip % radio input 
     const numberOfPeopleInput = document.querySelector(".people"); // no. of people input 
@@ -16,9 +18,9 @@ document.addEventListener("DOMContentLoaded", () => { // 1 wait til DOM is fully
 
     // converts entered values from strings into numbers 
 
-    let billValue = parseFloat(billInput.value) || 0; // this retrieves the value entered in the Bill field and stores it in the variable 'bill' 
-    let tipPercentage= parseFloat(tipInput.value) || 0; // this retrieves the value chosen from the tip amount buttons and stores it in 'tipInput'
-    let numberOfPeople = parseInt(numberOfPeopleInput.value, 10 || 1); // this retrieves the value entered in the number of people field and stores it in 'numberOfPeople'
+    let billValue = parseFloat(billInput.value) || 0; // this retrieves the value entered in the Bill field and stores it in the variable 'billValue'
+    let tipPercentage= parseFloat(tipInput.value) || 0; // this retrieves the value chosen from the tip amount buttons and stores it in 'tipPercentage'
+    let numberOfPeople = parseInt(numberOfPeopleInput.value, 10) || 1; // this retrieves the value entered in the number of people field and stores it in 'numberOfPeople'. We ask it to parse a number with radix Base 10 (decimals for money) as long as the outcome is truthy, otherwise use 1 person. 
 
     // does calculations necessary 
 
@@ -26,19 +28,26 @@ document.addEventListener("DOMContentLoaded", () => { // 1 wait til DOM is fully
     let tipAmountPerPerson = tipPercentage / numberOfPeople; 
     let totalPerPerson = (tipPercentage + billValue) / numberOfPeople; 
 
+    // update the totals to .2 decimal places accordingly
+
     tipAmountPerPerson.textContent = `$${tipAmountPerPerson.toFixed(2)}`;
     totalPerPerson.textContent = `$${totalPerPerson.toFixed(2)}`;
     // toFixed is a method to convert the number value to a fixed decimal point with the number of places defined in the brackets e.g. `(2)`.
 }
 
-    billInput.addEventListener('input', calculateTip); 
+    billInput.addEventListener('input', calculateTip); // is this scoped right? This logic isn't right. You can't calculate the tip till teh % has been selected, or you wouldn't. 
     numberOfPeopleInput.addEventListener('input', calculateTip);
     customTip.addEventListener('input', e => {
-
+        // this needs something here 
     })
+
+    // only when we have all three values do we want the calc to run 
+
+    // i think everything needs to be inside the function 
     tip.addEventListener('click', calculateTip);
+    // tip isn't anything 
 
     reset.addEventListener('click', e => { 
-
+    // reset the calc, clear all fields, don't calculate anything 
     })
 })
