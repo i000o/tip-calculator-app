c90555d37deedceaf65078a10021fbad27151d5f
i000o <iosman1096@gmail.com>
2026-06-08 21:34:55 +0100

    moved error function inside calc function, corrected .textContent to .value & used concantenation for readability on spans using .toFixed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // DEFINE VARIABLES 
 
     const billInput = document.querySelector(".bill"); // bill total input
-    const tipInput = document.querySelector(".tip"); // select tip % radio input 
+    const tipInput = document.querySelectorAll(".tip"); // select tip % radio input 
     const numberOfPeopleInput = document.querySelector(".people"); // no. of people input 
     const tipAmountPerPerson = document.querySelector(".tip-amount"); // show tip amount span 
     const totalPerPerson = document.querySelector(".total-per-person"); // show total bill per person span
@@ -17,41 +17,46 @@ document.addEventListener("DOMContentLoaded", () => {
     // 1) CALCULATE TIP 
     function calculateTip() { 
 
-    
-
     // CONVERT STRING INPUTS INTO USABLE VALUES 
     let billValue = parseFloat(billInput.value) || 0; // this retrieves the value entered in the Bill field and stores it in the variable 'billValue'
     let tipPercentage = parseFloat(tipInput.value) || 0; // this retrieves the value chosen from the tip amount buttons and stores it in 'tipPercentage'
     let numberOfPeople = parseInt(numberOfPeopleInput.value, 10) || 1; // this retrieves the value entered in the number of people field and stores it in 'numberOfPeople'. We ask it to parse a number with radix Base 10 (decimals for money) as long as the outcome is truthy, otherwise use 1 person. 
-    let customValue = parseFloat(customTip) || 0; 
+
+
+    function errorMessage() { // where should this be called? 
+        if (numberOfPeople === 0) { 
+            error.style.display = "block"; 
+        }
+    }
+
+    let customValue = parseFloat(customTip.value) || 0; 
+
 
     // CALCULATE TIP WITH THOSE VALUES 
     let tipValue = billValue * (tipPercentage / 100);
     let tipAmountPerPerson = tipValue / numberOfPeople; 
     let totalPerPerson = (tipValue + billValue) / numberOfPeople; 
 
-    // update DOM to show spans for tipAmountPerPerson and totalPerPerson  
-    // remember to use .toFixed(2) 
+    // remember to use .toFixed(2) Where does this go? 
 
     tipAmountPerPerson.style.display = ''; // added display: none; in CSS for x2 spans 
     totalPerPerson.style.display = ''; 
+    // replace the above 2 lines with the below 
 
+
+    tipAmountPerPerson.textContent = '$' + tipAmountValue.toFixed(2); 
+    totalPerPerson.textContent = '$' + totalPerPerson.toFixed(2);
     }
 
     // 2) RESET 
 
     function reset() { 
-        billInput.textContent = ""; // is it .value or .textContent? 
-        tipInput.textContent = ""; 
-        numberOfPeopleInput.textContent = ""; 
-        tipInput.textContent = false; 
+        billInput.value = ""; 
+        tipInput.value = ""; 
+        numberOfPeopleInput.value = ""; 
+        tipInput.value = false; 
     }
 
-    function errorMessage() { // where should this be called? 
-        if (numberOfPeople === 0) { 
-            error.display = "block"; // something like that 
-        }
-    }
 
     // EVENT LISTENERS 
 
@@ -61,4 +66,4 @@ document.addEventListener("DOMContentLoaded", () => {
     tipInput.addEventListener("input", calculateTip);
     numberOfPeopleInput.addEventListener("input", calculateTip);
     // error listener here? 
-})
+});
